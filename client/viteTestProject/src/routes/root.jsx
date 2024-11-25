08bd9524071e2610ccefb86f6c048b0bfd8da031
Leo Bradley <leobradley@Leos-MacBook-Pro.local>
2024-11-25 07:04:13 +0000

    add loading spinner component

--- client/viteTestProject/src/routes/root.jsx
+++ client/viteTestProject/src/routes/root.jsx
@@ -2,6 +2,12 @@ import { Link } from 'react-router-dom';
 
 
 export default function Root() {
-    return <Link to={'/users'}>Click here to go to the Users Application</Link>
+    return(
+      <>
+        <div className="center-div-margin-auto">
+        <Link to={'/users'}>Click here to go to the Users Application</Link>
+        </div>
+      </>
+    )
 
   }
--- client/viteTestProject/src/routes/userlist.jsx
+++ client/viteTestProject/src/routes/userlist.jsx
@@ -1,16 +1,22 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import UserNotFound from './userNotFound.jsx';
+import LoadingSpinner from './LoadingSpinner.jsx';
 
 
 export default function UserList() {
 
   const [users, setUsers] = useState([]);
-  
+  const [isDataLoading, setIsDataLoading] = useState(false);
+
   const fetchAPI = async () => {
+    setIsDataLoading(true);
     const response = await axios.get("http://localhost:8080/users");
     setUsers(response.data.users);
     console.log(response.data.users);
+
+    setIsDataLoading(false);
   };
 
   useEffect(() => {
@@ -19,7 +25,7 @@ export default function UserList() {
 
     return (
         <div>
-          {users.map((user) => (
+          {isDataLoading ? <LoadingSpinner/> : users.map((user) => (
             <div  key={user.id} className="flex flex-col gap-2">
               <Link   to={`/users/${user.id}`} >
                 {user.first_name} {user.last_name}
